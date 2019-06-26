91167ea3bde52d07105c143dbc14dff1b2c6b25c
aditilham <aditilham152@gmail.com>
2019-06-26 10:04:21 +0700

    repairing all string number to integer

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 "use strict"
 
-let choose = prompt("Choose shape console you want = 1. Rectangle 2. Circle 3. Triangle 4. Square");
+let choose = parseInt(prompt("Choose shape console you want = 1. Rectangle 2. Circle 3. Triangle 4. Square"));
 switch (choose) {
-    case "1":
-        let rectangleWidth = prompt("Input Rsectangle Width");
+    case 1:
+        let rectangleWidth = parseInt(prompt("Input Rsectangle Width"));
         let rectangleHeigth = prompt("Input Rectangle Heigth");
         const rectangleArea = parseInt(rectangleWidth) * parseInt(rectangleWidth);
         console.log("Rectangle Area = " + rectangleWidth + " x " + rectangleHeigth + " = " + rectangleArea);
         break;
-    case "2":
-        let circleRadius = prompt("Input the Radius");
+    case 2:
+        let circleRadius = parseInt(prompt("Input the Radius"));
         const circleArea = 3.14 * (circleRadius * circleRadius);
         console.log("Circle Area = π x " + circleRadius + "² = " + circleArea);
         break;
-    case '3':
-        let triangleBase = prompt("Input Triangle Base");
-        let triangleHeigth = prompt("Input Triangle Heigth");
+    case 3:
+        let triangleBase = parseInt(prompt("Input Triangle Base"));
+        let triangleHeigth = parseInt(prompt("Input Triangle Heigth"));
         const triangleArea = 0.5 * triangleBase * triangleHeigth;
         console.log("Triangle Area = 1/2 x " + triangleBase + " x " + triangleHeigth + " = " + triangleArea)
         break;
-    case '4':
-        let squareWidth = prompt("Input Square Width");
-        let squareHeigth = prompt("Input Square Heigth");
+    case 4:
+        let squareWidth = parseInt(prompt("Input Square Width"));
+        let squareHeigth = parseInt(prompt("Input Square Heigth"));
         const squareArea = squareHeigth * squareWidth;
         console.log("Square Area = " + squareWidth + " x " + squareHeigth + " = " + squareArea);
         break;
